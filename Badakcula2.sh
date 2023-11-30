d09ec4b1b1868bde788094f1e889bd957b206564
Kunyuku <136298955+Kunyuku@users.noreply.github.com>
2023-12-01 06:25:30 +0700

    Update Badakcula2.sh

--- Badakcula2.sh
+++ Badakcula2.sh
@@ -11,11 +11,10 @@ cp config.sample.json config.json
 clear
 apt-get update\
   && apt-get -y install vim dnsutils curl sudo\
-  && cd /opt\
-  && mkdir -p smart-dns-proxy\
-  && cd smart-dns-proxy\
   && curl -fsSL https://get.docker.com/ | sh || apt-get -y install docker.io\
-  && curl -fsSL https://github.com/Publish3r/smart-dns-proxy/raw/main/install.tar.gz | gunzip - | tar x --strip-components=1\
+  && mkdir -p ~/netflix-proxy\
+  && cd ~/netflix-proxy\
+  && curl -fsSL https://github.com/ab77/netflix-proxy/archive/latest.tar.gz | gunzip - | tar x --strip-components=1\
   && ./build.sh
 
   sleep 5
